a9fad289878b49c0ec58cf2decf08f4ff5cb2d00
Sami <52921619+sbagh@users.noreply.github.com>
2022-12-26 15:30:51 -0500

    Added files
    program foundation

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1- Create local storage
+
+link1 = { "www.youtube.com": 3 };
+
+chrome.storage.sync.set(link1);
+
+// 2- find current active tab from current focused window
+
+async function currentURL() {
+   const windowId = (await chrome.windows.getCurrent()).id;
+   const [tab] = await chrome.tabs.query({ active: true, windowId });
+   if (tab) {
+      return new URL(tab.url).hostname;
+   } else {
+      return "No tab found";
+   }
+}
+
+// 3- check if current host name is part of storage
+// if so return the countdown number (which is the correspdonding value of local storage key)
+
+let countdown = currentURL().then((url) => {
+   return chrome.storage.sync.get([url]).then((storageObject) => {
+      return storageObject[url];
+   });
+});
+
+// 4-trigger countdown
